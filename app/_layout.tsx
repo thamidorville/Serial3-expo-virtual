4eab2f694054089a37d470c2f35cba73d0720840
Felipe Gomes <bragagomesfelipe@outlook.com>
2026-03-30 15:50:52 -0300

    fix: corrigir nome da tela de objetos do tema no layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="[tema]/index" options={{ title: "Objetos do Tema" }} />
+      <Stack.Screen name="objetosTema" options={{ title: "Objetos do Tema" }} />
       <Stack.Screen name="exposicaoAR" options={{ title: "Exposição AR" }} />
     </Stack>
   )
